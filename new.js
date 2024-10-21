fceb80c37759e73827cb2563e57ebde2bc667000
Chiranjeevi sai charan <saicharan922014@gmail.com>
2024-10-21 20:04:37 +0530

    modified

---
+++ new.js
@@ -0,0 +1,20 @@
+// task1
+let str = "Hello World!";
+let newstr=str.replace("World!","My Dear Friend")
+console.log(newstr);
+//task2
+let a="Hungry";
+let b=a.repeat(10)
+console.log(b);
+//task3
+let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever.";
+let lastIndex=sentence.lastIndexOf("fox")
+console.log(lastIndex);
+//task4
+let message = "Hello, world!";
+let value=message.startsWith("hi")
+console.log(value);
+//task5
+let fileName = "document.pdf";
+let newv=fileName.endsWith(".pdf")
+console.log(newv);
